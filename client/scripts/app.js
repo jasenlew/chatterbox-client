02dca6063c710f9b6f1fa419eaa670899b930133
Hack Reactor Students <jasen.lew@gmail.com>
2014-06-16 21:10:34 -0700

    finished basic requirements

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,9 @@
 // YOUR CODE HERE:
 var App = function () {
-  this.server = "https://api.parse.com/1/classes/chatterbox";
+  this.server = "https://api.parse.com/1/classes/chatterbox?";
   this.friends = {};
   this.user;
+  this.rooms = {};
 };
 
 App.prototype.send = function (message) {
@@ -10,7 +11,7 @@ App.prototype.send = function (message) {
     url: this.server,
     type: "POST",
     data: JSON.stringify(message),
-    contentType: 'application/json',
+    contentType: "application/json",
     success: function (returnData) {
       console.log(returnData);
     },
@@ -21,14 +22,17 @@ App.prototype.send = function (message) {
 };
 
 App.prototype.fetch = function(){
+  var that = this;
   $.ajax({
     url: this.server,
     type: "GET",
+    data: {order: "-createdAt"},
+    // async: false,
     success: function (returnData) {
-      console.log(returnData);
+      that.populateChat(returnData);
     },
     error: function (returnData) {
-      console.log("FAILURE, yo: " + returnData);
+      console.log(returnData);
     }
   });
 };
@@ -37,28 +41,57 @@ App.prototype.clearMessages = function () {
   $("#chats").empty();
 };
 
-App.prototype.addMessage = function(message) {
+App.prototype.addMessage = function(message, roomname) {
   var li = '<li class="message">';
+  // SAVE OBJECT ID AS ID
   li += '<div class="username" id="' + message.username +'">' + message.username + '</div>';
   li += '<div class="text">' + message.text + '</div>';
   li += '</li>';
 
-  $("#chats").append(li);
+  // ONLY SAVE NEW MESSAGES
+  this.rooms[roomname].find('ul.messages').append(li);
 };
 
-App.prototype.addRoom = function(room){
-  var li = '<li class="room" id="'+room+'">' + room + '</li>';
-
-  $('#roomSelect').append(li);
+App.prototype.addRoom = function(roomname){
+    var li = '<li class="room" id="' + roomname + '">' + roomname + '</li>';
+    $('ul.rooms').append(li);
+    this.rooms[roomname] = $('li.room#'+roomname);
+    this.rooms[roomname].append('<ul class="messages"></ul>');
 };
 
 App.prototype.addFriend = function(username) {
   this.friends[username] = username;
 };
 
+App.prototype.populateChat = function(returnData){
+  console.log(returnData);
+  for(var i = 0; i < returnData.results.length; i++) {
+    var message = returnData.results[i];
+    //check to make sure its not an undefined roomname
+    if(message.roomname){
+      if (!this.rooms[message.roomname]) {
+        this.addRoom(message.roomname);
+      }
 
+      this.addMessage(message, message.roomname);
+
+      console.log("User: " + message.username);
+      console.log("Message: " + message.text);
+      console.log("Roomname: " + message.roomname);
+      console.log("-------");
+
+    }
+  }
+};
+
+
+/*--
+-----------------------------INITIALIZATION-----------------------
+ */
 App.prototype.init = function () {
   var that = this;
+
+  this.fetch();
 
   $('#main').find('.username').on('click', function(){
     var username =$(this).attr('id');
@@ -73,8 +106,9 @@ App.prototype.init = function () {
     };
     that.send(message);
   });
+
+  setInterval(function() {that.fetch()}, 1000);
 };
 
-
-
 var app = new App();
+app.init();
